aacaf6e776a88e3dfefe2ddaef66716b2e987aa6
tungarazaa <tungarazakennedy@gmail.com>
2025-02-18 12:50:56 +0300

    Removed dist from Git tracking and added to .gitignore

--- dist/controllers/productsController.js
+++
@@ -1,3 +0,0 @@
-export const getProducts = (req, res) => {
-    console.log("Kennedy is a hansome boy");
-};
--- dist/server.js
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config({ path: `${__dirname}/confing.env` });
-// const PORT = Number(process.env.PORT);
-console.log(process.env);
-// app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
